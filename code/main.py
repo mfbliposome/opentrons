2e48b5c2ad30335e047b4c70b024f00156e2af34
Eric <Eroginek@gmail.com>
2023-08-20 15:10:24 -0400

    Added comments to main.py, describing code logic

--- code/main.py
+++ code/main.py
@@ -1,11 +1,20 @@
 from preprocessing.excel_data_to_string import data_converter
 from preprocessing.protocols import Serial_Dilution_Tutorial
 
-XLSX_FILENAME ="./data/opentron template test example.xlsx"
+# Excel Workbook Data
+DATA_PATH = "./data"
+XLSX_FILENAME = f"{DATA_PATH}/opentron template test example.xlsx"
 SHEET1 = "Sheet1"
 SHEET2 = "Sheet2"
 
+# Data string obtained from Excel Workbook
 DATA = data_converter(XLSX_FILENAME, SHEET1, SHEET2)
+
+# Input File output data
+OUTPUT_PATH = "./input_files"
+OUTPUT_FILENAME = f"{OUTPUT_PATH}/serial_dilution_tutorial.py"
+
+# Create Input File and write to destination folder
 input_file = Serial_Dilution_Tutorial(DATA).input_file_generator()
-with open("./input_files/serial_dilution_tutorial.py", "w") as text_file:
+with open(OUTPUT_FILENAME, "w") as text_file:
     text_file.write(input_file)
